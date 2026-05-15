2d322f8c7949e5c383a7479fd77a24ba744f143d
Elaine Chen <elainechen1002edu@gmail.com>
2026-05-15 16:29:22 -0700

    Added Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
         <SiteShell>{children}</SiteShell>
 
         {process.env.NODE_ENV === "production" && <Analytics />}
+        <Analytics />
       </body>
     </html>
   )
